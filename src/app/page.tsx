9138d83fd474324ba67b0c861df6127c12e3cd4f
hugoglvs <104032491+hugoglvs@users.noreply.github.com>
2025-03-16 19:51:44 +0100

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white p-12">
+      <h1 className="text-4xl font-bold">My Interactive Portfolio</h1>
+    </main>
+  );
+}
